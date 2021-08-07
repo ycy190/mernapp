8093e896398a281590186f7dadcd3f196c4e8dee
ycy190 <ycy190@163.com>
2021-08-07 22:05:11 +1200

    heroku reset to node and port?

--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -34,7 +34,7 @@ class CreateBook extends Component {
     };
 
     axios
-      .post('http://localhost:8082/api/books', data)
+      .post('https://mernappsam.herokuapp.com:8080/api/books', data)
       .then(res => {
         this.setState({
           title: '',
