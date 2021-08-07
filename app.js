6b37e9787d86e0291fd7515020d5f85bdc3e41aa
ycy190 <ycy190@163.com>
2021-08-07 23:09:07 +1200

    heroku deplo

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const app = express();
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./mernappclient/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./mernappclient/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 // Connect Database
