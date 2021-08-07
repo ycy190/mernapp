5659de63c7fd0cd0cf16d19d1f2beec130c3221e
ycy190 <ycy190@163.com>
2021-08-08 00:04:33 +1200

    again..

--- client/src/components/ShowBookList.js
+++ client/src/components/ShowBookList.js
@@ -14,7 +14,7 @@ class ShowBookList extends Component {
 
   componentDidMount() {
     axios
-      .get('http://sammernapp.herokuapp.com:8089/api/books')
+      .get('http://sammernapp.herokuapp.com/api/books')
       .then(res => {
         this.setState({
           books: res.data
