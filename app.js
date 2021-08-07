1ddadf69438e18f8a44c788911809b4d74d0589d
ycy190 <ycy190@163.com>
2021-08-08 00:57:02 +1200

    maybe another tryss

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
-var cors = require('cors');
+const cors = require('cors');
 const PORT = process.env.PORT || 8089;
 
 // routes
@@ -12,11 +12,19 @@ const app = express();
 
 
 const path = require("path");
+app.use((req, res, next) => {
 
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next(); 
+
+});
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
