cafd059cdde360236c5d2394c4832e063890847e
ycy190 <ycy190@163.com>
2021-09-21 02:41:51 +1200

    load back s

--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -35,7 +35,7 @@ class CreateBook extends Component {
 
     
     axios
-      .post('http://sammernapp.herokuapp.com/api/books', data)
+      .post('https://sammernapp.herokuapp.com/api/books', data)
       .then(res => {
         this.setState({
           title: '',
